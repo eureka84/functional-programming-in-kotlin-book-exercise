a4e85a7adec740c36de69e99670ac517a0e774a7
Angelo Sciarra <angelosciarra@ymail.com>
2020-11-22 20:32:51 +0100

    [DONE] 3.28 reimplemented all tree methods in terms of fold

--- src/main/kotlin/org/eureka/kotlin/fp/ch3/Tree.kt
+++ src/main/kotlin/org/eureka/kotlin/fp/ch3/Tree.kt
@@ -5,7 +5,7 @@ sealed class Tree<out A> {
     companion object {
 
         fun <A> size(t: Tree<A>): Int = when (t) {
-                is Leaf -> 1
+            is Leaf -> 1
             is Branch -> 1 + size(t.left) + size(t.right)
         }
 
@@ -14,15 +14,36 @@ sealed class Tree<out A> {
             is Branch -> maxOf(maximum(t.left), maximum(t.right))
         }
 
-        fun <A> depth(t:Tree<A>): Int = when (t) {
+        fun <A> depth(t: Tree<A>): Int = when (t) {
             is Leaf -> 1
             is Branch -> 1 + maxOf(depth(t.left), depth(t.right))
         }
 
-        fun <A, B> map(t: Tree<A>, f: (A) ->B): Tree<B> = when(t) {
+        fun <A, B> map(t: Tree<A>, f: (A) -> B): Tree<B> = when (t) {
             is Leaf -> Leaf(f(t.value))
             is Branch -> Branch(map(t.left, f), map(t.right, f))
         }
+
+        fun <A, B> fold(ta: Tree<A>, l: (A) -> B, b: (B, B) -> B): B = when (ta) {
+            is Leaf -> l(ta.value)
+            is Branch -> b(fold(ta.left, l, b), fold(ta.right, l, b))
+        }
+
+        fun <A> sizeF(ta: Tree<A>): Int =
+            fold(ta, { 1 }, { l, r -> 1 + l + r })
+
+        fun maximumF(ta: Tree<Int>): Int =
+            fold(ta, { it }, { l, r -> maxOf(l, r) })
+
+        fun <A> depthF(ta: Tree<A>): Int =
+            fold(ta, { 1 }, { l, r -> 1 + maxOf(l, r) })
+
+        fun <A, B> mapF(ta: Tree<A>, f: (A) -> B): Tree<B> =
+            fold(ta, { leaf(f(it)) }, { l, r -> branch(l, r) })
+
+        fun <A> leaf(a: A): Tree<A> = Leaf(a)
+
+        fun <A> branch(l: Tree<A>, r: Tree<A>): Tree<A> = Branch(l, r)
 
     }
 
--- src/test/kotlin/org/eureka/kotlin/fp/ch3/TreeTest.kt
+++ src/test/kotlin/org/eureka/kotlin/fp/ch3/TreeTest.kt
@@ -3,9 +3,13 @@ package org.eureka.kotlin.fp.ch3
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import org.eureka.kotlin.fp.ch3.Tree.Companion.depth
+import org.eureka.kotlin.fp.ch3.Tree.Companion.depthF
 import org.eureka.kotlin.fp.ch3.Tree.Companion.map
+import org.eureka.kotlin.fp.ch3.Tree.Companion.mapF
 import org.eureka.kotlin.fp.ch3.Tree.Companion.maximum
+import org.eureka.kotlin.fp.ch3.Tree.Companion.maximumF
 import org.eureka.kotlin.fp.ch3.Tree.Companion.size
+import org.eureka.kotlin.fp.ch3.Tree.Companion.sizeF
 import org.junit.Test
 
 class TreeTest {
@@ -15,21 +19,25 @@ class TreeTest {
     @Test
     fun `size test`() {
         assertThat(size(t)).isEqualTo(5)
+        assertThat(sizeF(t)).isEqualTo(5)
     }
 
     @Test
     fun `maximum of`() {
         assertThat(maximum(t)).isEqualTo(3)
+        assertThat(maximumF(t)).isEqualTo(3)
     }
 
     @Test
     fun `depth of`() {
         assertThat(depth(t)).isEqualTo(3)
+        assertThat(depthF(t)).isEqualTo(3)
     }
 
     @Test
     fun mapTest() {
         val mapped = Branch(Branch(Leaf(2), Leaf(3)), Leaf(4))
         assertThat(map(t) { it + 1}).isEqualTo(mapped)
+        assertThat(mapF(t) { it + 1}).isEqualTo(mapped)
     }
 }
