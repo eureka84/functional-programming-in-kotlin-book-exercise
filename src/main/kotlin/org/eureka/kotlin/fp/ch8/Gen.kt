f7c33b8168019aa6be2539681684deedfe18ca39
Angelo Sciarra <angelosciarra@ymail.com>
2020-12-11 19:17:26 +0100

    8.8 Gen weighted

--- src/main/kotlin/org/eureka/kotlin/fp/ch8/Gen.kt
+++ src/main/kotlin/org/eureka/kotlin/fp/ch8/Gen.kt
@@ -5,6 +5,7 @@ import arrow.core.toTuple2
 import arrow.mtl.*
 import arrow.mtl.StateApi.just
 import org.eureka.kotlin.fp.ch6.RNG
+import org.eureka.kotlin.fp.ch6.double
 import org.eureka.kotlin.fp.ch6.nonNegativeInt
 
 data class Gen<A>(val sample: State<RNG, A>) {
@@ -23,6 +24,19 @@ data class Gen<A>(val sample: State<RNG, A>) {
 
         fun <A> union(ga: Gen<A>, gb: Gen<A>): Gen<A> =
             boolean().flatMap { b -> if (b) ga else gb }
+
+        fun <A> weighted(
+            pga: Pair<Gen<A>, Double>,
+            pgb: Pair<Gen<A>, Double>
+        ): Gen<A> {
+            val (genA, p1) = pga
+            val (genB, p2) = pgb
+
+            val ratio = p1 / (p2 + p1)
+
+            return Gen(State { rng -> double.run(rng).toTuple2().flip() })
+                .flatMap { d -> if (d < ratio) genA else genB }
+        }
 
         fun <A> listOfN(gn: Gen<Int>, ga: Gen<A>): Gen<List<A>> =
             gn.flatMap { n -> listOfN(n, ga) }
